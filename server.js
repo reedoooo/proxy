525a7dfba7ea9a287e44bff3f7d572635b7edb28
Reed Vogt <readvogt@gmail.com>
2023-12-29 01:40:25 -0800

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const fetch = require("node-fetch");
 const app = express();
-const PORT = 3001; // You can choose any available port
+const PORT = 3002; // You can choose any available port
 
 app.use(express.json());
 
